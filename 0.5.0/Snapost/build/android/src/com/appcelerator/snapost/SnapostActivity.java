df9e2bfe04c6d2e9fbf8f8f230764d4073e107c3
Kevin Whinnery <kevin@EVE.local>
2010-03-05 09:44:36 -0600

    removing older version

--- 0.5.0/Snapost/build/android/src/com/appcelerator/snapost/SnapostActivity.java
+++
@@ -1,7 +0,0 @@
-package com.appcelerator.snapost;
-
-import org.appcelerator.titanium.TitaniumActivityGroup;
-
-public class SnapostActivity extends TitaniumActivityGroup
-{
-}
--- 0.5.0/Snapost/build/android/src/com/appcelerator/snapost/SnapostApplication.java
+++
@@ -1,7 +0,0 @@
-package com.appcelerator.snapost;
-
-import org.appcelerator.titanium.TitaniumApplication;
-
-public class SnapostApplication extends TitaniumApplication {
-
-}
